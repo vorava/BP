a08724dc3ca233954d1ef402346552505f522451
vojta <vojta@ThinkPad-E14.localdomain>
2022-10-08 22:49:57 +0200

    latex vscode

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+import cv2
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    img = cv2.imread("FITwallpaper.png", 0)
+    cv2.imshow("A", img)
+    cv2.waitKey()
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
